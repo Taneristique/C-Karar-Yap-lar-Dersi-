2f48aada7dd4e5a291273d766b0797ce2ea87427
Taneristique <grkmtnrackz@outlook.fr>
2023-03-29 20:49:19 +0300

    updates

---
+++ Ortalama/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program{
+    static void Main(string[] args){
+        Console.WriteLine("2 adet sayı giriniz");
+        int not1=Convert.ToInt32(Console.ReadLine());
+        int not2=Convert.ToInt32(Console.ReadLine());
+        double ortalama=(not1+not2)/2;
+        if(ortalama>=50){
+            Console.WriteLine("Dersten geçtiniz.");
+        }
+        else{
+            Console.WriteLine("Dersten kaldiniz.");
+        }
+    }
+}
---
+++ dice/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program{
+    static void Main(){
+    Console.WriteLine("Lütfen 1 ve 6 arasında bir tam sayı girin : ");
+    int response=Convert.ToInt32(Console.ReadLine());
+    if(response<1||response>6){ 
+        throw new Exception("Response cannot be lower than 1 and higher than 6"); 
+    }
+    Console.WriteLine("Zar atılıyor....");
+    Random result=new Random();
+    int dice=result.Next(1,6);
+    if(dice==response){
+        Console.WriteLine("tebrikler doğru bildiniz!");
+    }
+    else{
+        Console.WriteLine("Yanıldınız bir daha deneyin!");
+    }
+    Console.ReadLine();
+        }
+    
+}
+
