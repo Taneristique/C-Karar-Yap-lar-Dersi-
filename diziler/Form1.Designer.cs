6e663af026103b93a6eacfc61089ae53436b62dd
Taneristique <grkmtnrackz@outlook.fr>
2023-04-19 14:41:58 +0300

    Date:      Wed Apr 19 14:41:58 2023 +0300  Changes to be committed: 	deleted:    diziler 	new file:   diziler/.gitignore 	new file:   diziler/Form1.resx 	new file:   diziler/Program.cs 	new file:   diziler/Properties/AssemblyInfo.cs 	new file:   diziler/Properties/Resources.Designer.cs 	new file:   diziler/Properties/Resources.resx 	new file:   diziler/Properties/Settings.Designer.cs 	new file:   diziler/Properties/Settings.settings
    	deleted:    diziler
    	new file:   diziler/.gitignore
    	new file:   diziler/App.config
    	new file:   diziler/Form1.Designer.cs
    	new file:   diziler/Form1.cs
    	new file:   diziler/Form1.resx
    	new file:   diziler/Program.cs
    	new file:   diziler/Properties/AssemblyInfo.cs
    	new file:   diziler/Properties/Resources.Designer.cs
    	new file:   diziler/Properties/Resources.resx
    	new file:   diziler/Properties/Settings.Designer.cs
    	new file:   diziler/Properties/Settings.settings
    	new file:   diziler/diziler.csproj
    	new file:   diziler/diziler.sln

---
+++ diziler/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace diziler
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.btnAraSayilar = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Location = new System.Drawing.Point(12, 367);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(207, 48);
+            this.btnGoster.TabIndex = 0;
+            this.btnGoster.Text = "Göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // btnAraSayilar
+            // 
+            this.btnAraSayilar.Location = new System.Drawing.Point(267, 367);
+            this.btnAraSayilar.Name = "btnAraSayilar";
+            this.btnAraSayilar.Size = new System.Drawing.Size(178, 48);
+            this.btnAraSayilar.TabIndex = 1;
+            this.btnAraSayilar.Text = "70 ve 85 Arasındaki Sayılar";
+            this.btnAraSayilar.UseVisualStyleBackColor = true;
+            this.btnAraSayilar.Click += new System.EventHandler(this.btnAraSayilar_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(481, 367);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "50\'den Küçük Sayılar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 9);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(207, 340);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(267, 9);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(178, 340);
+            this.listBox2.TabIndex = 4;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(481, 9);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(188, 340);
+            this.listBox3.TabIndex = 5;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 16;
+            this.listBox4.Location = new System.Drawing.Point(701, 12);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(184, 340);
+            this.listBox4.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(701, 367);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 48);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Küçükten Büyüğe Sırala";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(903, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(937, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Dizi Boyutu";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(942, 129);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(105, 29);
+            this.btnKaydet.TabIndex = 11;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1129, 450);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnAraSayilar);
+            this.Controls.Add(this.btnGoster);
+            this.Name = "Form1";
+            this.Text = "Dizi Uygulaması By TaNeRiStIqUe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.Button btnAraSayilar;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnKaydet;
+    }
+}
+
---
+++ diziler/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace diziler
+{
+    public partial class Form1 : Form
+    {
+        ArrayList ortalamalar = new ArrayList();
+        Random  number = new Random();
+        int diziBoyutu = 40; //Varsayılan dizi boyutu 40 olucak.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            for(int i = 0; i < diziBoyutu; i++)
+            {
+                ortalamalar.Add(Convert.ToDouble(number.NextDouble()*100));
+                listBox1.Items.Add(ortalamalar[i]);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bu bölüme dizinin eleman sayısını yazınız");
+        }
+
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+            ortalamalar.Clear();
+            listBox1.Items.Clear();
+            for (int i = 0; i < diziBoyutu; i++)
+            {
+                ortalamalar.Add(Convert.ToDouble(number.NextDouble() * 100));
+                listBox1.Items.Add(ortalamalar[i]);
+            }
+        }
+
+    
+
+        private void btnAraSayilar_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            foreach(double num in ortalamalar)
+            {
+                if(num >= 70 && num < 85)
+                {
+                    listBox2.Items.Add(num);
+                }
+            }
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+            foreach (double num in ortalamalar)
+            {
+                if (num < 50)
+                {
+                    listBox3.Items.Add(num);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox4.Items.Clear();
+            ortalamalar.Sort();
+
+            foreach (double num in ortalamalar)
+            {
+                listBox4.Items.Add(num);
+            }
+            
+
+
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            int value = Convert.ToInt32(textBox1.Text);
+            diziBoyutu = value;
+        }
+    }
+}
